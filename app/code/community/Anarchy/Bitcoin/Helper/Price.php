20a75752cc5b9a708a85db37b0bdfa1c84941007
Giulio Augusto Martinelli Guimarães <giulio.guimaraes@gmail.com>
2017-04-02 02:35:10 -0300

    I initialized the api bitcoinaverage implementation and the convention of currency values for bitcoin

---
+++ app/code/community/Anarchy/Bitcoin/Helper/Price.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Magento
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Open Software License (OSL 3.0)
+ * that is bundled with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://opensource.org/licenses/osl-3.0.php
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@magentocommerce.com so we can send you a copy immediately.
+ *
+ * @category    Anarchy
+ * @package     Anarchy_Bitcoin
+ * @copyright   Fuck the system _|_
+ * @license     http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
+class Anarchy_Bitcoin_Helper_Price extends Mage_Core_Helper_Abstract{
+    
+    public function getSubtotalBitcoin(){
+
+        $quote = Mage::getModel('checkout/session')->getQuote();
+        Zend_Debug::dump($quote->getData("subtotal"));
+    }     
+    
+}
